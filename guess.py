6e8c099e34057a3e114aba3d081c910839b4e3f8
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-10-03 12:42:20 -0400

    Get tries for every single player

--- guess.py
+++ guess.py
@@ -2,7 +2,7 @@ from telegram import Update,User
 from telegram.ext import Dispatcher,CommandHandler,CallbackContext
 import random
 randomNum = random.randint(1,99)
-times = 0
+tries = {}
 
 def guess(update, context):
     if len(context.args) == 0 :
@@ -12,32 +12,32 @@ def guess(update, context):
         """)
     else :
         global randomNum
-        global times
         if context.args[0].isdigit():
             number = int(context.args[0])
+            name = update.message.from_user.first_name
+            if name in tries:
+                tries[name] += 1
+            else:
+                tries[name] = 1
             if number < 100 :
                 if randomNum == number :
-                    times += 1
                     update.message.reply_text("""WOW! you guessed it! Good job, %s! You used %s times.
-哇！你猜到了！做得好，%s！你用了%s次。"""%(update.message.from_user.first_name, times, update.message.from_user.first_name, times))
+哇！你猜到了！做得好，%s！你用了%s次。"""%(name, tries[name], name, tries[name]))
                     randomNum = random.randint(1,99)
-                    times = 0
+                    if name in tries:
+                        tries[name] = 0
                 elif randomNum > number :
-                    times += 1
                     update.message.reply_text("""WRONG! %s, you are dumb. Can't you see the number's bigger you idiot??? Number of tries: %s
-错！ %s，你真傻。你看不出这个数字更大吗？你已经用了%s次了，你真差。"""%(update.message.from_user.first_name, times, update.message.from_user.first_name, times))
+错！ %s，你真傻。你看不出这个数字更大吗？你已经用了%s次了，你真差。"""%(name, tries[name], name, tries[name]))
                 elif randomNum < number :
-                    times += 1
-                    update.message.reply_text("""WRONG! %s, you are dumb. Can't you see the number's smaller you idiot??? Number of tries: %s
-错！ %s，你真傻。你看不出这个数字更小吗？你已经用了%s次了，你真差。"""%(update.message.from_user.first_name, times, update.message.from_user.first_name, times))
+                    update.message.reply_text("""WRONG! %s, you are dumb. Can't you see the number's smaller you idiot???  Number of tries: %s
+错！ %s，你真傻。你看不出这个数字更小吗？你已经用了%s次了，你真差。"""%(name, tries[name], name, tries[name]))
             else:
-                times += 1
                 update.message.reply_text("""WRONG! %s, you are dumb. I SAID BETWEEN 0 AND 100. Going back to elementry school? Number of tries: %s
-错！ %s，你真傻。我说的是0和100之间。要回去上小学喽？你已经用了%s次了，你真差。"""%(update.message.from_user.first_name, times, update.message.from_user.first_name, times))
+错！ %s，你真傻。我说的是0和100之间。要回去上小学喽？你已经用了%s次了，你真差。"""%(name, tries[name], name, tries[name]))
         else:
-            times += 1
             update.message.reply_text("""WRONG! %s, you are dumb. CAN'T YOU SEE %s IS NOT A NUMBER? Number of tries: %s          
-错！ %s，你真傻。你看不出 %s 都不是数字吗？你已经用了%s次了，你真差。"""%(update.message.from_user.first_name, context.args[0],times,update.message.from_user.first_name, context.args[0], times))
+错！ %s，你真傻。你看不出 %s 都不是数字吗？你已经用了%s次了，你真差。"""%(name, context.args[0], tries[name], name, context.args[0], tries[name]))
 
 def addHandler(dispatcher):
     guessHandler = CommandHandler('guess', guess)
