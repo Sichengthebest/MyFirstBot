c224f9d2219cf6bf5d2cef91776d006cb43172b9
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-10-08 09:08:23 -0400

    Change version

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from telegram.ext import CommandHandler, InlineQueryHandler, Updater
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 
 def start(update, context):
-    msg = "I'M THE GOD OF BOTS...\nVersion: 3.1.9\n我是机器人的上帝...\n版本：3.1.9"
+    msg = "I'M THE GOD OF BOTS...\nVersion: 3.2.9\n我是机器人的上帝...\n版本：3.2.9"
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def help(update, context):
