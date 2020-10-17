83915daf19c790228c4c62211db11151508ea5de
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-10-17 13:44:09 -0400

    Change version

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import bet
 from telegram.ext import CommandHandler, Updater
 
 def start(update, context):
-    msg = "I'M THE GOD OF BOTS...\n/help for commands.\nVersion: 4.2.2\n我是机器人的上帝...\n/help 来看命令。\n版本：4.2.2"
+    msg = "I'M THE GOD OF BOTS...\n/help for commands.\nVersion: 4.2.3\n我是机器人的上帝...\n/help 来看命令。\n版本：4.2.3x"
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def help(update, context):
