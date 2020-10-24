3faf761425bb4369782086e4986e374c1e349e2b
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-10-24 10:16:21 -0400

    change help

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="/start - Random command that makes the bot say \"I'm THE GOD OF BOTS...\" // 使机器人说“我是机器人的上帝”的随机命令。\n/hunt - Gain XP by catching animals. // 以捕捉动物的方式获得XP。\n/search - Go fetch the GP falling from the sky!!! // 去获取从天上掉下来的GP吧！！！\n/fish - Gain XP by fishing. // 以钓鱼的方式获得XP。\n/guessnum - Guess a number between 1 and 100. Try and do it with the least number of tries possible! // 猜一个0-100之间的数字。尝试尽可能减少尝试次数！\n/bet - Bet a number rolled by two dice! $$$... // 赌一个由两个骰子掷出的数字！ $$$ ...")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="/start - Random command that makes the bot say \"I'm THE GOD OF BOTS...\" // 使机器人说“我是机器人的上帝”的随机命令。\n/hunt - Gain XP by catching animals. // 以捕捉动物的方式获得XP。\n/search - Go fetch the GP falling from the sky!!! // 去获取从天上掉下来的GP吧！！！\n/fish - Gain XP by fishing. // 以钓鱼的方式获得XP。\n/guessnum - Guess a number between 1 and 100. Try and do it with the least number of tries possible! // 猜一个0-100之间的数字。尝试尽可能减少尝试次数！\n/bet - Bet a number rolled by two dice! $$$... // 赌一个由两个骰子掷出的数字！ $$$...")
 def read_file_as_str(file_path):
     # 判断路径文件存在
     if not os.path.isfile(file_path):
