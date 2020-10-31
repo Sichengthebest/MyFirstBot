e5f105391796af5ca5fdf6ec1325d8b91ec3e3e9
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-10-31 09:53:15 -0400

    Add curruid

--- bet.py
+++ bet.py
@@ -2,15 +2,6 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Dispatcher, CommandHandler, CallbackQueryHandler
 import random
 
-button1 = InlineKeyboardButton("9 and more / 比九大", callback_data='more than nine')
-button2 = InlineKeyboardButton("6 or 8 / 六或八", callback_data='six or eight')
-button3 = InlineKeyboardButton("7", callback_data='seven')
-button4 = InlineKeyboardButton("5 and less / 比五小", callback_data='less than five')
-butoon6 = InlineKeyboardButton("Surrender / 逃跑", callback_data='flee')
-button6 = InlineKeyboardButton("Start game / 开始", callback_data='ok')
-
-keyb = InlineKeyboardMarkup([[button4,button2,button3,button1],[butoon6, button6]])
-
 randomNum1 = random.randint(1,6)
 randomNum2 = random.randint(1,6)
 sum = randomNum1 + randomNum2
@@ -30,62 +21,76 @@ msg = """I rolled two dice. Please choose one button, then press Start Game to c
 """
 
 def bet(update, context):
+    uid = update.effective_user.id
+    button1 = InlineKeyboardButton("9 and more / 比九大", callback_data='bet:more than nine-%s'%uid)
+    button2 = InlineKeyboardButton("6 or 8 / 六或八", callback_data='bet:six or eight-%s'%uid)
+    button3 = InlineKeyboardButton("7", callback_data='bet:seven-%s'%uid)
+    button4 = InlineKeyboardButton("5 and less / 比五小", callback_data='bet:less than five-%s'%uid)
+    butoon6 = InlineKeyboardButton("Surrender / 逃跑", callback_data='bet:flee-%s'%uid)
+    button6 = InlineKeyboardButton("Start game / 开始", callback_data='bet:ok-%s'%uid)
+
+    keyb = InlineKeyboardMarkup([[button4,button2,button3,button1],[butoon6, button6]])
     global msg
     update.message.reply_text("%s"%msg, reply_markup = keyb)
 
 def betCallback(update, context):
     global sum,randomNum1,randomNum2,thing,record,recordCH
+    place, curruid = update.callback_query.data.split('-')
+    uid = update.effective_user.id
     query = update.callback_query
-    if query.data == 'less than five' and sum < 6:
+    if str(uid) != curruid:
+        query.answer("你是谁？你在哪儿？你想做啥？这是别人的，大笨蛋！",show_alert=True)
+        return
+    if place == 'bet:less than five' and sum < 6:
         thing = 1
         query.answer("You chose less than five\n您选择了比五小", show_alert=True)
         record = "less than five"
         recordCH = "比五小"
-    elif query.data == 'less than five' and sum > 5:
+    elif place == 'bet:less than five' and sum > 5:
         thing = 2
         query.answer("You chose less than five\n您选择了比五小", show_alert=True)
         record = "less than five"
         recordCH = "比五小"
-    elif query.data == 'six or eight' and sum == 6:
+    elif place == 'bet:six or eight' and sum == 6:
         thing = 1
         query.answer("You chose six or eight\n您选择了六或八", show_alert=True)
         record = "six or eight"
         recordCH = "六或八"
-    elif query.data == 'six or eight' and sum == 8:
+    elif place == 'bet:six or eight' and sum == 8:
         thing = 1
         query.answer("You chose six or eight\n您选择了六或八", show_alert=True)
         record = "six or eight"
         recordCH = "六或八"
-    elif query.data == 'six or eight' and not sum == 6 and not sum == 8:
+    elif place == 'bet:six or eight' and not sum == 6 and not sum == 8:
         thing = 2
         query.answer("You chose six or eight\n您选择了六或八", show_alert=True)
         record = "six or eight"
         recordCH = "六或八"
-    elif query.data == 'seven' and sum == 7:
+    elif place == 'bet:seven' and sum == 7:
         thing = 1
         query.answer("You chose seven\n您选择了七", show_alert=True)
         record = "seven"
         recordCH = "七"
-    elif query.data == 'seven' and not sum == 7:
+    elif place == 'bet:seven' and not sum == 7:
         thing = 2
         query.answer("You chose seven\n您选择了七", show_alert=True)
         record = "seven"
         recordCH = "七"
-    elif query.data == 'more than nine' and sum > 8:
+    elif place == 'bet:more than nine' and sum > 8:
         thing = 1
         query.answer("You chose more than nine\n您选择了比九大", show_alert=True)
         record = "more than nine"
         recordCH = "比九大"
-    elif query.data == 'more than nine' and sum < 9:
+    elif place == 'bet:more than nine' and sum < 9:
         thing = 2
         query.answer("You chose more than nine\n您选择了比九大", show_alert=True)
         record = "more than nine"
         recordCH = "比九大"
-    elif query.data == 'flee':
+    elif place == 'bet:flee':
         query.answer("You coundn't afford to lose, that's why you gave 10GP to the casino staff to run away.\n您输不起，这就是为什么您给赌场工作人员10GP为了逃走。", show_alert=True)
         query.edit_message_text("You coundn't afford to lose, that's why you gave 10GP to the casino staff to run away.\n您输不起，这就是为什么您给赌场工作人员10GP为了逃走。")
         thing = 0
-    elif query.data == 'ok':
+    elif place == 'bet:ok':
         if thing == 0:
             query.answer("You haven't chosen anything\n您还没有选择任何东西")
         elif thing == 1:
@@ -112,4 +117,4 @@ def betCallback(update, context):
 def addHandler(dispatcher):
     betHandler = CommandHandler('bet', bet)
     dispatcher.add_handler(betHandler)
-    dispatcher.add_handler(CallbackQueryHandler(betCallback))
+    dispatcher.add_handler(CallbackQueryHandler(betCallback,pattern="^bet:[A-Za-z0-9_]*"))
