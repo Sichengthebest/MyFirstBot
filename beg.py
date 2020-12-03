39227406fc1b389d65373ce1a1d423f6ae131a53
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-12-02 19:10:43 -0500

    Add gifs

--- beg.py
+++ beg.py
@@ -26,34 +26,34 @@ def beg(update,context):
         return
     if index == 0:
         rwbotgod = random.randint(10,250)
-        update.message.reply_text("%s donated %s GP to you!\n%s 给了你 %s GP!\nCreator/作者: Sichengthebest"%(ppl[index],rwbotgod,pplCH[index],rwbotgod))
+        update.message.reply_animation('https://media3.giphy.com/media/xT5LMuVBDfoBDtf3tS/giphy.gif',caption="%s donated %s GP to you!\n%s 给了你 %s GP!\nCreator/作者: Sichengthebest"%(ppl[index],rwbotgod,pplCH[index],rwbotgod))
         coins.add_coins(user,rwbotgod)
     elif index == 1:
         rwsicheng = random.randint(50,400)
-        update.message.reply_text("%s donated %s GP to you!\n%s 给了你 %s GP!\nCreator/作者: Sichengthebest"%(ppl[index],rwsicheng,pplCH[index],rwsicheng))
+        update.message.reply_animation('https://media3.giphy.com/media/xT5LMuVBDfoBDtf3tS/giphy.gif',caption="%s donated %s GP to you!\n%s 给了你 %s GP!\nCreator/作者: Sichengthebest"%(ppl[index],rwsicheng,pplCH[index],rwsicheng))
         coins.add_coins(user,rwsicheng)
     elif index == 2 or index == 3 or index == 5 or index == 6 or index == 7 or index == 10 or index == 11:
         if yesorno == 1:
             rwbrad = random.randint(10,100)
-            update.message.reply_text("%s donated %s GP to you!\n%s 给了你 %s GP!\nCreator/作者: Sichengthebest"%(ppl[index],rwbrad,pplCH[index],rwbrad))
+            update.message.reply_animation('https://media3.giphy.com/media/xT5LMuVBDfoBDtf3tS/giphy.gif',caption="%s donated %s GP to you!\n%s 给了你 %s GP!\nCreator/作者: Sichengthebest"%(ppl[index],rwbrad,pplCH[index],rwbrad))
             coins.add_coins(user,rwbrad)
         else:
-            update.message.reply_text("%s: %s\n%s: %s\nCreator/作者: Sichengthebest"%(ppl[index],badstuff[index],pplCH[index],badstuffCH[index]))
+            update.message.reply_animation('https://media3.giphy.com/media/xT5LMuVBDfoBDtf3tS/giphy.gif',caption="%s: %s\n%s: %s\nCreator/作者: Sichengthebest"%(ppl[index],badstuff[index],pplCH[index],badstuffCH[index]))
     elif index == 4:
-        update.message.reply_text("%s: STOP THE STEAL! STOP THE STEAL! STOP THE STEAL!\n%s: 停止窃取！停止窃取！停止窃取！\nCreator/作者: Sichengthebest"%(ppl[index],pplCH[index]))
+        update.message.reply_animation('https://media3.giphy.com/media/xT5LMuVBDfoBDtf3tS/giphy.gif',caption="%s: STOP THE STEAL! STOP THE STEAL! STOP THE STEAL!\n%s: 停止窃取！停止窃取！停止窃取！\nCreator/作者: Sichengthebest"%(ppl[index],pplCH[index]))
     elif index == 8:
-        amongus = ["Ur sus","You died","Shut up, or I'll sabotage the lights"]
-        amongusCH = ["我怀疑你","你死了","闭嘴，不然的话我会把灯关上"]
+        amongus = ["Ur sus","You died","Shut up and go fix the wiring in Electrical"]
+        amongusCH = ["我怀疑你","你死了","闭嘴然后去修修电站的电线"]
         auindex = random.randint(0,2)
-        update.message.reply_text("%s: %s\n%s: %s\nCreator/作者: Sichengthebest"%(ppl[index],amongus[auindex],pplCH[index],amongusCH[auindex]))
+        update.message.reply_animation('https://media3.giphy.com/media/xT5LMuVBDfoBDtf3tS/giphy.gif',caption="%s: %s\n%s: %s\nCreator/作者: Sichengthebest"%(ppl[index],amongus[auindex],pplCH[index],amongusCH[auindex]))
         if auindex == 1:
             coins.add_hp(user,-100)
     elif index == 9:
-        update.message.reply_text("%s: I'm your mom, and too bad if ur poor\n%s: 我是你老妈，如果你穷，那就太可惜了\nCreator/作者: Sichengthebest"%(ppl[index],pplCH[index]))
+        update.message.reply_animation('https://media3.giphy.com/media/xT5LMuVBDfoBDtf3tS/giphy.gif',caption="%s: I'm your mom, and too bad if ur poor\n%s: 我是你老妈，如果你穷，那就太可惜了\nCreator/作者: Sichengthebest"%(ppl[index],pplCH[index]))
     elif index == 12:
-        update.message.reply_text("%s: Bruh I need money why would you want some from me\n%s: 你真noah，我需要钱，为什么你还要找我\nCreator/作者: Sichengthebest"%(ppl[index],pplCH[index]))
+        update.message.reply_animation('https://media3.giphy.com/media/xT5LMuVBDfoBDtf3tS/giphy.gif',caption="%s: Bruh I need money why would you want some from me\n%s: 你真noah，我需要钱，为什么你还要找我\nCreator/作者: Sichengthebest"%(ppl[index],pplCH[index]))
     elif index == 13:
-        update.message.reply_text("%s: No cuz I'm evil\n%s: 不，因为我很坏\nCreator/作者: Sichengthebest"%(ppl[index],pplCH[index]))
+        update.message.reply_animation('https://media3.giphy.com/media/xT5LMuVBDfoBDtf3tS/giphy.gif',caption="%s: No cuz I'm evil\n%s: 不，因为我很坏\nCreator/作者: Sichengthebest"%(ppl[index],pplCH[index]))
     gametimes[user] = datetime.now() + timedelta(seconds=45)
     
 
