c5e63070a24808606e10e65e5c9be61ba224c3b5
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-12-11 15:53:02 -0500

    Change help

--- bot.py
+++ bot.py
@@ -22,10 +22,10 @@ def help(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="""Here's all my commands. Have fun!
 这是我的所有命令。玩得开心点！
 -------------------------------------------------
-Start commmand:
+Start commmand // 启动命令:
 /start - Random command that makes the bot say \"I'm THE GOD OF BOTS...\" // 使机器人说 “我是机器人的上帝” 的随机命令。
 -------------------------------------------------
-Currency commands:
+Currency commands // 货币命令:
 /hunt - Gain beastcoins by catching animals. // 以捕捉动物的方式获得兽币。
 /huntbal - Check the amount of beastcoins you have. // 检查您有多少兽币。
 /huntshop - Buy nice useful stuff for catching more animals! // 购买帮忙捕捉动物的东西！
@@ -39,17 +39,17 @@ Currency commands:
 /convert - Convert one currency into another! // 将一种货币转换为另一种货币！
 /bal - Check the amount of money you have. // 检查您有多少GP。
 -------------------------------------------------
-Inventory commands:
+Inventory commands // 库存命令:
 /buy - Buy nice useful stuff! // 购买有用的东西！
 /sell - Sell useless stuff. // 出售无用的东西。
 /inv - Check the items you have in your inventory. // 检查库存中的物品。
 -------------------------------------------------
-Bank commands:
+Bank commands // 银行命令:
 /dep - Deposit money from your wallet to your bank! // 从钱包里存钱到银行！
 /withdraw - Withdraw money from your bank to your wallet! // 从银行提款！
 /banknote - Increase the amount of GP you can stuff into your bank! // 增加您可以存入银行的GP数量！
 -------------------------------------------------
-Fun commands:
+Fun commands // 有趣的命令:
 /guessnum - Guess a number between 1 and 100. Try and do it with the least number of tries possible! // 猜一个0-100之间的数字。尽量减少尝试次数！
 /capitals - How good are you at capitals? // 你了解所有首都吗？
 /gif - The bot will send you a funny gif. // 机器人会向您发送有趣的GIF。
