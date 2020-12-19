d031bdc457c0902c69be9f54624daab71c396015
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-12-18 19:06:22 -0500

    Add shell

---
+++ shell/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+systemctl --user start sichenggodbot_service
+systemctl --user status sichenggodbot_service
---
+++ shell/stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+systemctl --user stop sichenggodbot_service
+systemctl --user status sichenggodbot_service
