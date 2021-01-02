a0b5e47c4c6534077d794fb17aee7ff9ed605082
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-01-02 10:15:13 -0500

    Update info.py

--- info.py
+++ info.py
@@ -2,14 +2,26 @@ from telegram.ext import Dispatcher,CommandHandler
 from json import dumps
 
 def ainfo(update,context):
-    u = str(update['message']['reply_to_message']['animation'])
-    u = dumps(eval(u),indent=4)
-    update.message.reply_text(u)
+    if update.message.reply_to_message:
+        if update.message.reply_to_message.animation:
+            u = str(update['message']['reply_to_message']['animation'])
+            u = dumps(eval(u),indent=4)
+            update.message.reply_text(u)
+        else:
+            update.message.reply_text("You need to reply this to an animation!")
+    else:
+        update.message.reply_text("You need to reply!")
 
 def pinfo(update,context):
-    u = str(update['message']['reply_to_message']['photo'][0])
-    u = dumps(eval(u),indent=4)
-    update.message.reply_text(u)
+    if update.message.reply_to_message:
+        if update.message.reply_to_message.animation:
+            u = str(update['message']['reply_to_message']['photo'][0])
+            u = dumps(eval(u),indent=4)
+            update.message.reply_text(u)
+        else:
+            update.message.reply_text("You need to reply this to a photo!")
+    else:
+        update.message.reply_text("You need to reply!")
 
 def add_handler(dp):
     dp.add_handler(CommandHandler("ainfo", ainfo))
