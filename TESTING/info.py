b534df9756e22e2ffdcd3b8d6f83c3ac48abb556
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-01-09 10:34:21 -0500

    Update info.py

--- TESTING/info.py
+++ TESTING/info.py
@@ -1,27 +1,6 @@
 from telegram.ext import Dispatcher,CommandHandler
 from json import dumps
 
-def ainfo(update,context):
-    if update.message.reply_to_message:
-        if update.message.reply_to_message.animation:
-            u = str(update['message']['reply_to_message']['animation'])
-            u = dumps(eval(u),indent=4)
-            update.message.reply_text(u)
-        else:
-            update.message.reply_text("You need to reply this to an animation!")
-    else:
-        update.message.reply_text("You need to reply!")
-
-def pinfo(update,context):
-    if update.message.reply_to_message:
-        if update.message.reply_to_message.animation:
-            u = str(update['message']['reply_to_message']['photo'][0])
-            u = dumps(eval(u),indent=4)
-            update.message.reply_text(u)
-        else:
-            update.message.reply_text("You need to reply this to a photo!")
-    else:
-        update.message.reply_text("You need to reply!")
 
 def info(update,context):
     if update.message.reply_to_message:
