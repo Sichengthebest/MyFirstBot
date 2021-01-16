34c66e2b9958d87671ea642b612ecab82a648768
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-01-16 17:30:37 -0500

    Update youtube_music.py

--- youtube_music.py
+++ youtube_music.py
@@ -1,7 +1,7 @@
 import pafy
 import os
 from telegram.ext import CommandHandler, Updater
-from telegram import BotCommand
+from telegram import BotCommand, InputMediaAudio, bot
 
 def yt(update,context):
     if len(context.args) == 1:
@@ -11,12 +11,20 @@ def yt(update,context):
             return
         video = pafy.new(url)
         bestaudio = video.getbestaudio(preftype="m4a")
+        music_size = bestaudio.get_filesize()
+        if music_size > 10000000:
+            update.message.reply_text("Sorry, but that video is too long, I can only download videos smaller that 10MB!")
+            return
+        msg = update.message.reply_photo('https://i.pcmag.com/imagery/articles/04oP7J3OIykTchX4vhU57vn-28..1569485834.jpg',caption=f"Dowloading... Please be patient. Your video is {music_size/1000} KBs long")
         filepath = f"/tmp/{bestaudio.title}.{bestaudio.extension}"
         bestaudio.download(filepath=filepath)
-        update.message.reply_audio(open(filepath, 'rb'))
+        msg.edit_media(InputMediaAudio(open(filepath, 'rb')))
         os.remove(filepath)
     else:
         update.message.reply_text("Incorrect arguments! Correct format: /getytm {youtube url}")
+
+def getCommand():
+    return [BotCommand('getytm', 'Download some music from a Youtube url!')]
 
 def addHandler(dispatcher):
     dispatcher.add_handler(CommandHandler('getytm', yt))
