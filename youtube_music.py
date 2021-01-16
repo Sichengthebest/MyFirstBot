08cd67caba866ff2c88fa3408ebfc762353d0cb4
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-01-16 10:01:34 -0500

    Update youtube_music.py

--- youtube_music.py
+++ youtube_music.py
@@ -5,17 +5,18 @@ from telegram import BotCommand
 def yt(update,context):
     if len(context.args) == 1:
         url = context.args[0]
+        _,_,prefix,_ = url.split('/')
+        if not prefix == 'www.youtube.com':
+            update.message.reply_text("Incorrect arguments! The url must come from Youtube!")
+            return
         video = pafy.new(url)
-        update.message.reply_text(f"{video}")
+        bestaudio = video.getbestaudio(preftype="m4a")
+        filepath = f"/tmp/{bestaudio.title}.{bestaudio.extension}"
+        bestaudio.download(filepath=filepath)
+        update.message.reply_audio(open(filepath, 'rb'))
+        os.remove(filepath)
     else:
-        update.message.reply_text("请告诉我URL啊，不然我去给你变啊？！")
+        update.message.reply_text("Incorrect arguments! Correct format: /getytm {youtube url}")
 
 def addHandler(dispatcher):
-    dispatcher.add_handler(CommandHandler('ytmusic', yt))
-    
-if __name__ == "__main__":
-    url = "https://www.youtube.com/watch?v=C7Tl7Zn-KRE"
-    video = pafy.new(url)
-    bestaudio = video.getbestaudio(preftype="m4a")
-    filepath = f"/tmp/{bestaudio.title}.{bestaudio.extension}"
-    bestaudio.download(filepath=filepath)
+    dispatcher.add_handler(CommandHandler('getytm', yt))
