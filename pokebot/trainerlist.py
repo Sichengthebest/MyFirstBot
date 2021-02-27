bea3fca48aeb05bf8342d62be163e0038b873e5a
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-02-27 14:59:31 -0500

    Add the first three trainers

--- pokebot/trainerlist.py
+++ pokebot/trainerlist.py
@@ -1,6 +1,122 @@
 trainers = {
     '001': {
         'name': 'Brock',
+        'party': [{
+            "id": "074",
+            "xp": 11000,
+            "friendship": -1,
+            "moves": ['Tackle','Defence Curl']
+        },{
+            "id": "095",
+            "xp": 13000,
+            "friendship": -1,
+            "moves": ['Tackle','Screech','Bide','Bind']
+        }],
+        'quotes': {
+            'start': "I'm Brock! I'm Pewter's Gym Leader! I believe in rock hard defense and determination! That's why my Pokémon are all the Rock-type! Do you still want to challenge me? Fine then! Show me your best!",
+            'win': "As proof of your victory, take an official Pokémon League Badge—a Boulder Badge!\nWith a Boulder Badge, Pokémon up to Lv. 20 from the wild will listen to you without question!",
+            'lose': "The best offense is a good defense! That's my way of doing things!"
+        },
+    },
+    '002': {
+        'name': 'Misty',
+        'party': [{
+            "id": "120",
+            "xp": 17000,
+            "friendship": -1,
+            "moves": ['Tackle','Water Gun']
+        },{
+            "id": "121",
+            "xp": 20000,
+            "friendship": -1,
+            "moves": ['Tackle','Water Gun','Harden','Bubble Beam']
+        }],
+        'quotes': {
+            'start': "Hi, you're a new face! Trainers who want to turn pro have to have a policy about Pokémon! What is your approach when you catch Pokémon? My policy is an all-out offensive with Water-type Pokémon!",
+            'win': "I can't believe I lost! All right! You can have the Cascade Badge to show you beat me!\nThe Cascade Badge makes all Pokémon up to L30 obey! That includes even outsiders!",
+            'lose': "See! This is the Water-type toughness I was talking about!"
+        }
+    },
+    '003': {
+        'name': 'Lt. Surge',
+        'party': [{
+            "id": "100",
+            "xp": 20000,
+            "friendship": -1,
+            "moves": ['Tackle','Screech','Sonic Boom']
+        },{
+            "id": "025",
+            "xp": 17000,
+            "friendship": -1,
+            "moves": ['Thunder Shock','Thunder Wave','Growl','Quick Attack']
+        },{
+            "id": "026",
+            "xp": 23000,
+            "friendship": -1,
+            "moves": ['Thunderbolt','Thunder Shock','Thunder Wave','Growl']
+        }],
+        'quotes': {
+            'start': "Hey, kid! What do you think you're doing here? You won't live long in combat! That's for sure! I tell you kid, electric Pokémon saved me during the war! They zapped my enemies into paralysis! The same as I'll do to you!",
+            'win': "Whoa! You're the real deal, kid! Fine then, take the Thunder Badge!\nThe Thunder Badge makes all pokemon from Level 40 and up to obey!",
+            'lose': "Oh yeah! I'm strong!\nOh yeah! When it comes to Electric-type Pokémon, I'm number one in the world!"
+        }
+    },
+    '004': {
+        'name': 'Erika',
+        'party': [],
+        'quotes': {
+            'start': "I'm Brock! I'm Pewter's Gym Leader! I believe in rock hard defense and determination! That's why my Pokémon are all the Rock-type! Do you still want to challenge me? Fine then! Show me your best!",
+            'win': "As proof of your victory, take an official Pokémon League Badge—a Boulder Badge!\nWith a Boulder Badge, Pokémon up to Lv. 20 from the wild will listen to you without question!",
+            'lose': "The best offense is a good defense! That's my way of doing things!"
+        }
+    },
+    '005': {
+        'name': 'Koga',
+        'party': [],
+        'quotes': {
+            'start': "I'm Brock! I'm Pewter's Gym Leader! I believe in rock hard defense and determination! That's why my Pokémon are all the Rock-type! Do you still want to challenge me? Fine then! Show me your best!",
+            'win': "As proof of your victory, take an official Pokémon League Badge—a Boulder Badge!\nWith a Boulder Badge, Pokémon up to Lv. 20 from the wild will listen to you without question!",
+            'lose': "The best offense is a good defense! That's my way of doing things!"
+        }
+    },
+    '006': {
+        'name': 'Sabrina',
+        'party': [],
+        'quotes': {
+            'start': "I'm Brock! I'm Pewter's Gym Leader! I believe in rock hard defense and determination! That's why my Pokémon are all the Rock-type! Do you still want to challenge me? Fine then! Show me your best!",
+            'win': "As proof of your victory, take an official Pokémon League Badge—a Boulder Badge!\nWith a Boulder Badge, Pokémon up to Lv. 20 from the wild will listen to you without question!",
+            'lose': "The best offense is a good defense! That's my way of doing things!"
+        }
+    },
+    '007': {
+        'name': 'Blaine',
+        'party': [],
+        'quotes': {
+            'start': "I'm Brock! I'm Pewter's Gym Leader! I believe in rock hard defense and determination! That's why my Pokémon are all the Rock-type! Do you still want to challenge me? Fine then! Show me your best!",
+            'win': "As proof of your victory, take an official Pokémon League Badge—a Boulder Badge!\nWith a Boulder Badge, Pokémon up to Lv. 20 from the wild will listen to you without question!",
+            'lose': "The best offense is a good defense! That's my way of doing things!"
+        }
+    },
+    '008': {
+        'name': 'Giovanni',
+        'party': [],
+        'quotes': {
+            'start': "I'm Brock! I'm Pewter's Gym Leader! I believe in rock hard defense and determination! That's why my Pokémon are all the Rock-type! Do you still want to challenge me? Fine then! Show me your best!",
+            'win': "As proof of your victory, take an official Pokémon League Badge—a Boulder Badge!\nWith a Boulder Badge, Pokémon up to Lv. 20 from the wild will listen to you without question!",
+            'lose': "The best offense is a good defense! That's my way of doing things!"
+        }
+    },
+    '101': {
+        'name': 'Brdsdvfdsfsdfock',
+        'party': [],
+        'quotes': {
+            'start': "I'm Brock! I'm Pewter's Gym Leader! I believe in rock hard defense and determination! That's why my Pokémon are all the Rock-type! Do you still want to challenge me? Fine then! Show me your best!",
+            'win': "As proof of your victory, take an official Pokémon League Badge—a Boulder Badge!\nWith a Boulder Badge, Pokémon up to Lv. 20 from the wild will listen to you without question!",
+            'lose': "The best offense is a good defense! That's my way of doing things!"
+        }
+    },
+    '111': {
+        'name': 'Brdsdvf57475347dsfsdfock',
         'party': [],
         'quotes': {
             'start': "I'm Brock! I'm Pewter's Gym Leader! I believe in rock hard defense and determination! That's why my Pokémon are all the Rock-type! Do you still want to challenge me? Fine then! Show me your best!",
